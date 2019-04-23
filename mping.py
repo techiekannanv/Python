7fdf5514591dd8f0dff635987f5987d875a02639
techiekannanv <techiekannanv@gmail.com>
2019-04-23 09:42:01 +0530

    args group added

--- mping.py
+++ mping.py
@@ -5,8 +5,6 @@ import lookup
 import pingip
 import argparse
 import multiprocessing as mp
-import time
-import logging
 
 
 limits = 10
@@ -15,32 +13,34 @@ def worker(ip,count, timeout):
     resolve = list(lookup.Lookup(ip))
     print("{}:{}:{}".format(resolve[0],resolve[1], pingip.Ping(resolve[0], count, timeout)))
 
-parse = argparse.ArgumentParser(description='This script is used to ping multiple ips in parallel')
-parse.add_argument('-f', action='store', dest='file', default=None)
-parse.add_argument('-i', action='store', dest='ip', default=None)
-parse.add_argument('-c', action='store', dest='count', default=3)
-parse.add_argument('-W', action='store', dest='timeout', default=1)
-parse.add_argument('-fmt', action='store', dest='format', default='line')
-args = parse.parse_args()
-ips = []
-#mp.log_to_stderr(logging.INFO)
-if ( args.file == None and args.ip == None ):
-    parse.print_help()
+if __name__ == '__main__':
+    parse = argparse.ArgumentParser(description='This script is used to ping multiple ips in parallel')
+    group = parse.add_mutually_exclusive_group()
+    group.add_argument('-f', action='store', dest='file', default=None, help="List of IP's")
+    group.add_argument('-i', action='store', dest='ip', default=None, help="IP to ping")
+    parse.add_argument('-c', action='store', dest='count', default=3, help="No.of packets to send")
+    parse.add_argument('-W', action='store', dest='timeout', default=1, help="timeout in ms to wait for each reply")
+    args = parse.parse_args()
+    ips = []
 
-if ( args.file != None ):
-    with open(args.file,'r') as file:
-        for line in file:
-            ips.append(line.strip())
+    if ( args.file == None and args.ip == None ):
+        parse.print_help()
 
-if (args.ip != None):
-    ips.append(args.ip)
-procs = []
-while ( ips ):
-    if ( len(mp.active_children()) < limits ):
-        proc = mp.Process(target=worker, args=(ips.pop(),args.count, args.timeout))
-        procs.append(proc)
-        proc.start()
+    if ( args.file != None ):
+        with open(args.file,'r') as file:
+            for line in file:
+                ips.append(line.strip())
 
-for proc in procs:
-    if proc.is_alive():
-        proc.join()
+    if (args.ip != None):
+        ips.append(args.ip)
+
+    procs = []
+    while ( ips ):
+        if ( len(mp.active_children()) < limits ):
+            proc = mp.Process(target=worker, args=(ips.pop(),args.count, args.timeout))
+            procs.append(proc)
+            proc.start()
+
+    for proc in procs:
+        if proc.is_alive():
+            proc.join()
